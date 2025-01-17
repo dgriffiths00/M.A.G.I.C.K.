7ee74c365938e9ae5ea9d68b70a09b01059a0a1e
cgoddard_INFO <cgoddard@confederationcollege.ca>
2025-01-17 09:51:51 -0500

    Add project files.

---
+++ So You Wanna Make A Dnd Character/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace So_You_Wanna_Make_A_Dnd_Character
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
