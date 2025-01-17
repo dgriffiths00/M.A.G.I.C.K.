fead4a385c61527629620547a342a838f68b0e30
cgoddard_INFO <cgoddard@confederationcollege.ca>
2025-01-17 10:35:55 -0500

    Basic unfinished class structure, and start of figuring out the drop down box

---
+++ So You Wanna Make A Dnd Character/ClassesFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace So_You_Wanna_Make_A_Dnd_Character
+{
+    class Character
+    {
+        //this will have all the info for the character
+        //it will have a class, race, background, stats etc etc
+
+
+        //stats, created via method
+        private int STR, DEX, CON, SMRT, WIS, CHA;
+        private string name, background;
+
+        
+
+        //constructors will one for full and one for completely empty
+        //the one for all constructor will also have options to fill in blank ones, and if == null then blank
+
+
+    }
+
+
+    abstract class DndClass
+    {
+        //this will be inhearented by all the classes
+
+
+    }
+
+    abstract class DndRace
+    {
+        //this will be inherented by all the races
+
+
+    }
+
+}
--- So You Wanna Make A Dnd Character/MainForm.Designer.cs
+++ So You Wanna Make A Dnd Character/MainForm.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ClassPickerBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // ClassPickerBox
+            // 
+            this.ClassPickerBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ClassPickerBox.FormattingEnabled = true;
+            this.ClassPickerBox.Location = new System.Drawing.Point(92, 97);
+            this.ClassPickerBox.Name = "ClassPickerBox";
+            this.ClassPickerBox.Size = new System.Drawing.Size(147, 24);
+            this.ClassPickerBox.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(712, 648);
+            this.Controls.Add(this.ClassPickerBox);
             this.Name = "MainForm";
             this.Text = "So You Want To Make A DND Character";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox ClassPickerBox;
     }
 }
 
