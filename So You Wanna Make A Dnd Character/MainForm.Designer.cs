7171a3ac42861d56242b7346673b8a50cfa651fb
cgoddard_INFO <cgoddard@confederationcollege.ca>
2025-01-17 10:18:31 -0500

    Just First commit

--- So You Wanna Make A Dnd Character/MainForm.Designer.cs
+++ So You Wanna Make A Dnd Character/MainForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(712, 648);
+            this.Name = "MainForm";
+            this.Text = "So You Want To Make A DND Character";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
