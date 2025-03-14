df3ca5a04d3c0c85d2f4b60792aa03bd4be2830e
cgoddard_INFO <cgoddard@confederationcollege.ca>
2025-03-14 13:48:13 -0400

    Added first sql query

--- So You Wanna Make A Dnd Character/MainForm.cs
+++ So You Wanna Make A Dnd Character/MainForm.cs
@@ -33,8 +33,6 @@ namespace M_A_G_I_C_K
             //making the drop boxes default to the select please.. to allow for reselection of nothing after a selected option
             RaceDropBox.SelectedIndex = 0;
             ClassDropBox.SelectedIndex = 0;
-
-            Console.WriteLine(Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName) + @"\Databases\Primary Database.db");
         }
 
         //this will change the spells you can pick etc etc based on what you pick
