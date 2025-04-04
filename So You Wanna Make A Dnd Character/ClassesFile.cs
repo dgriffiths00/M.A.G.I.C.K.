a7730481ee8f04c636c350ce2be0d1525fa461c9
Duncan Griffiths <duncangriffiths2000@gmail.com>
2025-04-03 22:29:23 -0400

    calculateSpellStats method working, is called in calculatingStats method IF character is a spellcaster, if not, it isnt called.

--- So You Wanna Make A Dnd Character/ClassesFile.cs
+++ So You Wanna Make A Dnd Character/ClassesFile.cs
@@ -462,6 +462,25 @@ namespace M_A_G_I_C_K
                         break;
                 }
             }
+            //Testing code to check the make sure values are updated properly.
+
+            //Console.WriteLine("-----------------");
+            //Console.WriteLine("");
+            //Console.WriteLine("AC: " + _AC);
+            //Console.WriteLine("HP: " + _CharClass.Hitpoints);
+            //Console.WriteLine("CON " + _StatBonus[2]);
+            //Console.WriteLine("Dex " + _StatBonus[1]);
+            //if (_SpellCaster != null)
+            //{
+
+            //    Console.WriteLine("Spellcasting Modifier (Save): " + _SpellCaster.spellSaveDC);
+            //    Console.WriteLine("Spellcasting Modifier (Attack Bonus): " + _SpellCaster.SpellAtkBonus);
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("-----------------");
+
+
+
         }
         public void creatingPdf()
         {
